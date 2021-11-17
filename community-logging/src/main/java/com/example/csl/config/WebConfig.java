f043a88f277792246d26e8289703177f016742b4
Chenplushao <675295312@qq.com>
2021-11-18 01:55:23 +0800

    csl更新logging模块

--- community-logging/src/main/java/com/example/csl/config/WebConfig.java
+++ community-logging/src/main/java/com/example/csl/config/WebConfig.java
@@ -6,17 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.annotation.Resource;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Resource
     private TokenInterceptor tokenInterceptor ;
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**")
-                .excludePathPatterns("/swagger-ui.html")
-                .excludePathPatterns("/swagger-resources/**")
-                .excludePathPatterns("/error")
-                .excludePathPatterns("/webjars/**")
-        .excludePathPatterns("/user/**");
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
     }
 }
 
